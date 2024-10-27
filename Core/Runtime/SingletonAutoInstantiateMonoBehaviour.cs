aa4b620e6b1cac8e2e77cbbab050fed0114489dc
Khanh Nguyen Dinh <khanhnd@ikameglobal.com>
2024-10-27 09:19:05 +0700

    upt

---
+++ Core/Runtime/SingletonAutoInstantiateMonoBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Squirrel
+{
+    public abstract class SingletonAutoInstantiateMonoBehaviour<T> : MonoBehaviour where T : SingletonAutoInstantiateMonoBehaviour<T>
+    {
+        private static T _instance;
+
+        public static T gInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
